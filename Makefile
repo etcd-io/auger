cdd5e2c1be6fd31ff3d63072a1f707cfe26f68da
Chun-Hung Tseng <henrybear327@gmail.com>
2024-04-01 08:27:37 +0200

    Add Makefile targets for running fmt and static analysis checks
    Changes:
    - Introduce `make verify` and `make fmt` targets in Makefile
    - For linter, use the same config as etcd repo
    - Update GitHubCI workflow accordingly
    
    Closes #31
    
    References:
    - fmt and lint https://github.com/etcd-io/bbolt/blob/c57b3537ed933f2e613f04d42688d49e511f69b3/Makefile#L30C1-L36C84
    - fmt script https://github.com/etcd-io/etcd/blob/a08812eb01572aa64479543d1e8fa33cda22b8b8/scripts/fix.sh
    - GithubCI https://github.com/etcd-io/etcd/blob/a08812eb01572aa64479543d1e8fa33cda22b8b8/.github/workflows/static-analysis.yaml
    - linter config https://github.com/etcd-io/etcd/blob/a08812eb01572aa64479543d1e8fa33cda22b8b8/tools/.golangci.yaml
    
    Signed-off-by: Chun-Hung Tseng <henrybear327@gmail.com>

--- Makefile
+++ Makefile
@@ -19,6 +19,19 @@ GO_VERSION ?= 1.21.8
 GOOS ?= linux
 GOARCH ?= amd64
 TEMP_DIR := $(shell mktemp -d)
+GOFILES = $(shell find . -name \*.go)
+
+.PHONY: fmt
+fmt:
+	@echo "Verifying gofmt, failures can be fixed with ./scripts/fix.sh"
+	@!(gofmt -l -s -d ${GOFILES} | grep '[a-z]')
+
+	@echo "Verifying goimports, failures can be fixed with ./scripts/fix.sh"
+	@!(go run golang.org/x/tools/cmd/goimports@latest -l -d ${GOFILES} | grep '[a-z]')
+
+.PHONY: verify
+verify:
+	golangci-lint run ./...
 
 # Local development build
 build:
---
+++ scripts/fix.sh
@@ -0,0 +1,10 @@
+GO_CMD="go"
+
+GOFILES=$(${GO_CMD} list  --f "{{with \$d:=.}}{{range .GoFiles}}{{\$d.Dir}}/{{.}}{{\"\n\"}}{{end}}{{end}}" ./...)
+TESTGOFILES=$(${GO_CMD} list  --f "{{with \$d:=.}}{{range .TestGoFiles}}{{\$d.Dir}}/{{.}}{{\"\n\"}}{{end}}{{end}}" ./...)
+XTESTGOFILES=$(${GO_CMD} list  --f "{{with \$d:=.}}{{range .XTestGoFiles}}{{\$d.Dir}}/{{.}}{{\"\n\"}}{{end}}{{end}}" ./...)
+
+echo "${GOFILES}" "${TESTGOFILES}" "${XTESTGOFILES}"| xargs -n 100 go run golang.org/x/tools/cmd/goimports@latest -w -local go.etcd.io
+
+go fmt ./...
+go mod tidy
